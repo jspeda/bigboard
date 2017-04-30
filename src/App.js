064aa01efd5ac3369599c4106cf34f6651628df8
Joseph Spedale <jspedale@gmail.com>
2017-04-29 21:41:31 -0400

    Added function to retrieve scores fo all games.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentWillMount() {
+
+    const scoreBoard = fetch('http://gd2.mlb.com/components/game/mlb/year_2017/month_04/day_26/miniscoreboard.json');
+    scoreBoard
+      .then(data => data.json())
+      .then(data => {
+        console.log(data.data.games.game)
+        data.data.games.game.map(game => console.log(
+          `${game.away_team_city}: ${game.away_team_runs} vs ${game.home_team_city}: ${game.home_team_runs} status: ${game.status}`
+          // I also would want to grab: the link to the game, outs, inning, bottom or top, runs hits errors
+        ))
+      })
+
+}
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
