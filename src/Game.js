6e9c2aea5c3ce1b4b2130aaa98e4429d64ac054d
Joseph Spedale <jspedale@gmail.com>
2017-05-05 23:11:33 -0400

    No longer displaying inning for games that have ended. Also deleted superfluous code.

--- src/Game.js
+++ src/Game.js
@@ -31,17 +31,12 @@ class Game extends Component {
             <div className="inning-number">
               {
                 // might need to change this to deal with final
-                gameData.inning != undefined ?
+                gameData.inning != undefined && gameData.status != 'Final' ?
                   (gameData.top_inning === "Y" ? `${String.fromCharCode(8593)} ${gameData.inning}` : `${String.fromCharCode(8595)} ${gameData.inning}`) :
                   null
               }
             </div>
           </div>
-          {/* <div className="headers"><div className="team-header">team</div><div className="runs-header">r</div><div className="hits-header">h</div><div className="errors-header">e</div></div>
-          <div className="away-team"><div className="away-name">{gameData.away_file_code}</div><div className="away-runs">{gameData.away_team_runs}</div><div className="away-hits">{gameData.away_team_hits}</div><div className="away-errors">{gameData.away_team_errors}</div></div>
-          <div className="home-team"><div className="home-name">{gameData.home_file_code}</div><div className="away-runs">{gameData.home_team_runs}</div><div className="away-hits">{gameData.home_team_hits}</div><div className="away-errors">{gameData.home_team_errors}</div></div> */}
-          {/* only show status if it's "Delayed Start", "Postponed", "Warmup", or "Preview" */}
-          {/* <div className="status">{gameData.status}</div> */}
         </div>
         <div className="status">
           {
